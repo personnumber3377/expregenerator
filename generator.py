c16e577480fc6a7913d885f0525a82d2cbc3aab9
personnumber3377 <sontapaa.jokulainen@gmail.com>
2023-10-28 22:18:38 +0300

    Added string operations too! :)

--- generator.py
+++ generator.py
@@ -13,14 +13,58 @@ COMPARISON_TYPES = [["<", 1], ["<=", 1], [">", 1], [">=", 1], ["=", 1], ["==", 1
 LOGIC_TYPES = [["|", 1], ["&", 1]]
 
 
+'''
 
-NODE_TYPES = [["+", 1], ["-", 1], ["*", 1], ["%", 1], ["placeholder", 2]]+LOGIC_TYPES+COMPARISON_TYPES # This list is of the format [OPERATOR, UNARY/BINARY] (Binary operation is one and unary is zero and two means number)
+       match STRING REGEXP
+              same as STRING : REGEXP
+
+       substr STRING POS LENGTH
+              substring of STRING, POS counted from 1
+
+       index STRING CHARS
+              index in STRING where any CHARS is found, or 0
+
+       length STRING
+              length of STRING
+
+'''
+
+
+STRING_OPERATIONS = [[":", 1], ["substr", 1], ["index", 1], ["length", 1]]
+
+STRING_OPS = [":", "substr", "index", "length"]
+
+
+
+
+NODE_TYPES = [["+", 1], ["-", 1], ["*", 1], ["%", 1], ["placeholder", 2]]+LOGIC_TYPES+COMPARISON_TYPES+STRING_OPERATIONS # This list is of the format [OPERATOR, UNARY/BINARY] (Binary operation is one and unary is zero and two means number)
+
 
 
 
 MAX_NUM = 100000
 
 ZERO_CHANCE = 0.2 # This is a special case for the number zero. The number zero is special because it has a lot of edge cases so I have put a 30 percent chance for it to occur.
+
+
+
+
+
+alphabet = "abcdefghijklmnopqrstuvxyz"
+
+def random_str(length: int):
+	return ''.join([random.choice(alphabet) for _ in range(length)])
+
+
+
+def rand_substr(string: str):
+	if len(string) < 1:
+		return string
+	start = random.randrange(len(string)-1)
+	end = random.randrange(start, len(string)-1)
+
+	return string[start:end]
+
 
 class ExprTree:
 	def __init__(self):
@@ -37,7 +81,7 @@ class ExprTree:
 
 class ExprNode:
 	
-	def __init__(self, cur_layer, node_type=None, max_layer=MAX_DEPTH):
+	def __init__(self, cur_layer, node_type=None, max_layer=MAX_DEPTH, string_argument=False):
 		
 		if node_type == None:
 			node_type = random.choice(NODE_TYPES)
@@ -45,6 +89,7 @@ class ExprNode:
 		self.left = None
 		self.right = None
 		self.val = None
+		self.is_string_op = False
 
 		binary = node_type[1]
 
@@ -59,6 +104,12 @@ class ExprNode:
 		operation = node_type[0]
 
 		self.val = operation # assign operand to the value of this node.
+		
+
+		if self.val in STRING_OPS:
+			self.is_string_op = True
+			self.val = operation
+			return
 
 		override = None
 		if cur_layer == max_layer - 1:
@@ -74,6 +125,24 @@ class ExprNode:
 
 	def get_value(self):
 		
+		if self.is_string_op:
+			if self.val == ":":
+				poostring = random_str(random.randrange(1,10))
+				return "( "+random_str(random.randrange(1,10))+str(poostring)+" : "+poostring+" )" # Make random ass regex
+			elif self.val == "substr": #substr STRING POS LENGTH
+
+				rand_str = random_str(random.randrange(1,10)) 
+				return  "( substr "+rand_str+" "+str(random.randrange(1,10))+" "+str(random.randrange(1,4))+" )"
+			elif self.val == "index": # index STRING CHARS
+
+				rand_str = random_str(random.randrange(1,10)) 
+
+				return "( index "+rand_str+" "+rand_substr(rand_str)+" )"
+			elif self.val == "length":
+				return "( length "+str(random_str(random.randrange(1,10)))+" )"
+
+
+
 		if self.left == None and self.right == None: # Just get the value of this node because terminal node.
 			#if self.val < 0:
 
